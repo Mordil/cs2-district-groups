b77c725804d5de417cd37eb03ecc8c3795203d94
Nathan Harris <2nd.lt.harris@gmail.com>
2026-08-22 14:47:16 -0500

    [Dev] Delete unused files

--- src/UI/src/fpsCounter/bindings.ts
+++
@@ -1,5 +0,0 @@
-import { bindValue } from "cs2/api"
-import mod from "../../mod.json"
-
-// UI-only (no persistence)
-export const debugShowFpsCounter$ = bindValue<boolean>(mod.id, "debugShowFpsCounter", false)
