4819fe27a1ac9a9cfdb111d04073b28758bcf9d0
Nathan Harris <2nd.lt.harris@gmail.com>
2026-08-24 23:43:39 -0500

    [Localization] Refactor EN dictionary to use constant keys

--- src/Code/Localization/LocalizationKey.cs
+++ src/Code/Localization/LocalizationKey.cs
@@ -1,7 +1,65 @@
 namespace DistrictGroups
 {
+    /*
+        Shared locale-id constants for the custom mod UI. Keys must match locale.ts's id() exactly
+
+        Setting-panel entries are, as their locale ids are already generated consistently by other APIs
+    */
     public static class LocalizationKey
     {
+        public const string PanelTitle = "DistrictGroups.UI[PanelTitle]";
+        public const string NewGroupButton = "DistrictGroups.UI[NewGroupButton]";
+        public const string NewGroupButtonTooltip = "DistrictGroups.UI[NewGroupButtonTooltip]";
+        public const string NewGroupDefaultName = "DistrictGroups.UI[NewGroupDefaultName]";
+        public const string FilterTooltipLine1 = "DistrictGroups.UI[FilterTooltipLine1]";
+        public const string AllGroupsLabel = "DistrictGroups.UI[AllGroupsLabel]";
+        public const string NoGroupsYet = "DistrictGroups.UI[NoGroupsYet]";
+        public const string NoGroupsMatchFilter = "DistrictGroups.UI[NoGroupsMatchFilter]";
+        public const string DisplayDistrictAreasLabel = "DistrictGroups.UI[DisplayDistrictAreasLabel]";
+        public const string ShowGroupOverlayLabel = "DistrictGroups.UI[ShowGroupOverlayLabel]";
+
+        public const string DeleteGroupTooltipLine1 = "DistrictGroups.UI[DeleteGroupTooltipLine1]";
+        public const string DeleteGroupTooltipLine2 = "DistrictGroups.UI[DeleteGroupTooltipLine2]";
+        public const string TypePickerTooltipLine1 = "DistrictGroups.UI[TypePickerTooltipLine1]";
+        public const string TypePickerTooltipLine2 = "DistrictGroups.UI[TypePickerTooltipLine2]";
+        public const string TypePickerTooltipLine3 = "DistrictGroups.UI[TypePickerTooltipLine3]";
+        public const string DeleteGroupConfirmMessage = "DistrictGroups.UI[DeleteGroupConfirmMessage]";
+        public const string DeleteGroupDialogTitle = "DistrictGroups.UI[DeleteGroupDialogTitle]";
+        public const string DeleteGroupConfirm = "DistrictGroups.UI[DeleteGroupConfirm]";
+        public const string DeleteGroupCancel = "DistrictGroups.UI[DeleteGroupCancel]";
+        public const string RemoveMemberTooltip = "DistrictGroups.UI[RemoveMemberTooltip]";
+        public const string SelectDistrictsButton = "DistrictGroups.UI[SelectDistrictsButton]";
+        public const string GroupColorTooltip = "DistrictGroups.UI[GroupColorTooltip]";
+
+        public const string ToggleTooltipTitle = "DistrictGroups.UI[ToggleTooltipTitle]";
+        public const string ToggleTooltipBody = "DistrictGroups.UI[ToggleTooltipBody]";
+
+        public const string SectionTooltipLine1 = "DistrictGroups.UI[SectionTooltipLine1]";
+        public const string SectionTooltipLine2 = "DistrictGroups.UI[SectionTooltipLine2]";
+        public const string SectionTooltipLine3 = "DistrictGroups.UI[SectionTooltipLine3]";
+        public const string SectionTooltipLine4 = "DistrictGroups.UI[SectionTooltipLine4]";
+        public const string SectionLabel = "DistrictGroups.UI[SectionLabel]";
         public const string OperatingDistrictsLabel = "DistrictGroups.UI[OperatingDistrictsLabel]";
+        public const string UnassignOption = "DistrictGroups.UI[UnassignOption]";
+        public const string UnassignTooltipDisabled = "DistrictGroups.UI[UnassignTooltipDisabled]";
+        public const string UnassignTooltipEnabled = "DistrictGroups.UI[UnassignTooltipEnabled]";
+        public const string UnassignedLabel = "DistrictGroups.UI[UnassignedLabel]";
+        public const string GroupSearchTitle = "DistrictGroups.UI[GroupSearchTitle]";
+        public const string SearchGroupsPlaceholder = "DistrictGroups.UI[SearchGroupsPlaceholder]";
+        public const string NoGroupsMatchSearch = "DistrictGroups.UI[NoGroupsMatchSearch]";
+
+        public const string TypeGeneric = "DistrictGroups.UI[TypeGeneric]";
+        public const string TypePolice = "DistrictGroups.UI[TypePolice]";
+        public const string TypeFire = "DistrictGroups.UI[TypeFire]";
+        public const string TypeHealthcare = "DistrictGroups.UI[TypeHealthcare]";
+        public const string TypeDeathcare = "DistrictGroups.UI[TypeDeathcare]";
+        public const string TypeGarbage = "DistrictGroups.UI[TypeGarbage]";
+        public const string TypeEducationElementary = "DistrictGroups.UI[TypeEducationElementary]";
+        public const string TypeEducationHighSchool = "DistrictGroups.UI[TypeEducationHighSchool]";
+        public const string TypeEducationCollege = "DistrictGroups.UI[TypeEducationCollege]";
+        public const string TypeEducationUniversity = "DistrictGroups.UI[TypeEducationUniversity]";
+        public const string TypePost = "DistrictGroups.UI[TypePost]";
+        public const string TypeParks = "DistrictGroups.UI[TypeParks]";
+        public const string TypeWelfare = "DistrictGroups.UI[TypeWelfare]";
     }
 }
